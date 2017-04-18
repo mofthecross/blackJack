e3d54e47246895b09504c094fd0c892069f8ab79
mofthecross <mofthecross@gmail.com>
2017-04-18 16:43:37 -0700

    Implement Deck class with build method

--- client/tools.js
+++ client/tools.js
@@ -4,7 +4,24 @@ class Card {
     this.value = value;
   }
 }
-class Deck {}
+
+class Deck {
+  constructor() {
+    this.cards = [];
+  }
+  build() {
+    const cardValues = [2,3,4,5,6,7,8,9,10,'A','JACK', 'QUEEN','KING'];
+    const suites = ['Clubs','Hearts', 'Diamonds', 'Spades'];
+
+    cardValues.forEach( cardValue => {
+      suites.forEach(suite => {
+        const newCard = new Card(suite, cardValue);
+        this.cards.push(newCard);
+      });
+    });
+    //return this.cards;
+  }
+}
 
 class Player {}
 
@@ -12,9 +29,14 @@ class Dealer {}
 
 class Game {}
 
-/* test */
+/* test
 
 const testCardClass = new Card('Diamonds', 8);
 console.log('testCardClass', testCardClass);
 console.log('testCardClass value should be 8:', testCardClass.value === 8);
 console.log('testCardClass suite should be Diamonds:', testCardClass.suite === 'Diamonds');
+*/
+const testDeckClass = new Deck();
+console.log('testCardClass', testDeckClass);
+testDeckClass.build();
+console.log('testCardClass should build the cards array', testDeckClass.cards.length === 52);
