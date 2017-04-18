c9a07634e3d478820a3569214197c4c7d096faa7
mofthecross <mofthecross@gmail.com>
2017-04-18 16:27:10 -0700

    Implement Card class

--- client/tools.js
+++ client/tools.js
@@ -1,5 +1,9 @@
-class Card {}
-
+class Card {
+  constructor(suite, value) {
+    this.suite = suite;
+    this.value = value;
+  }
+}
 class Deck {}
 
 class Player {}
@@ -7,3 +11,10 @@ class Player {}
 class Dealer {}
 
 class Game {}
+
+/* test */
+
+const testCardClass = new Card('Diamonds', 8);
+console.log('testCardClass', testCardClass);
+console.log('testCardClass value should be 8:', testCardClass.value === 8);
+console.log('testCardClass suite should be Diamonds:', testCardClass.suite === 'Diamonds');
